6ebb1639e4360244b007b113e017e8c3df0d892e
Stephen Stephenson <stephenson.inbox@gmail.com>
2025-03-04 04:53:42 +0100

    Update run file to use config flag

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@
 # Get the project root
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-./bin/leaked-passwords-checker
+./bin/leaked-passwords-checker --config="./configs/leaked-passwords-checker.json"
