4626991ef33e5d2debcbebb9bac72b193aaad850
Stephen Stephenson <stephenson.inbox@gmail.com>
2025-03-05 02:28:36 +0100

    Improve download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,4 +3,8 @@
 # Get the project root
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Navigate to the project root
+PROJECT_ROOT="$SCRIPT_DIR/.."
+cd "$PROJECT_ROOT" || exit 1
+
 ./bin/downloader --config="./configs/leaked-passwords-checker.json"
